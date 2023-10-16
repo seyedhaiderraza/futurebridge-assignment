a486fc1edc333694ab39a5df64dbee17044a62ea
seyedhaiderraza <seyedhaiderraza@gmail.com>
2023-10-16 22:57:17 +0530

    basic  setup

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
-
+const dotenv = require("dotenv").config();
 const app = express();
 
 app.get("/", (req, resp) => {
   resp.send("server homepage");
 });
 
-app.listen(3006, (err) => {
-  console.log("server started");
+app.listen(process.env.PORT, (err) => {
+  console.log("server started at", process.env.PORT);
 });
